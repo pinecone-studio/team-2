fd66c2399fbb63e7b85fc43167ac993a3b8364ca
Khishigdari <nbhishgee22@gmail.com>
2026-03-09 15:03:24 +0800

    library setup

---
+++ libs/shadcn/src/lib/shadcn.tsx
@@ -0,0 +1,11 @@
+import styles from './shadcn.module.css';
+
+export function Shadcn() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Shadcn!</h1>
+    </div>
+  );
+}
+
+export default Shadcn;
