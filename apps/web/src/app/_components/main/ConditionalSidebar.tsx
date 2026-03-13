039c88ea205d77796a3f9f23feaa9d7198725ecd
Khishigdari <nbhishgee22@gmail.com>
2026-03-13 08:39:31 +0800

    sidebar

--- apps/web/src/app/_components/main/ConditionalSidebar.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-import { useAuth } from '@clerk/nextjs';
-import Sidebar from '../../sidebar/page';
-
-export function ConditionalSidebar() {
-  const { isSignedIn } = useAuth();
-  return isSignedIn ? <Sidebar /> : null;
-}
