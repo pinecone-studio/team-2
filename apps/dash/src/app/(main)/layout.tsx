679efd81afebb6c076005cb310ecc678fdc42297
Khishigdari <nbhishgee22@gmail.com>
2026-03-19 15:37:25 +0800

    admin access only to sign nin added

--- apps/dash/src/app/(main)/layout.tsx
+++ apps/dash/src/app/(main)/layout.tsx
@@ -2,6 +2,7 @@
 
 import { SidebarProvider } from '@team/source-ui';
 import { usePathname } from 'next/navigation';
+import { AdminGuard } from '../_components/AdminGuard';
 import { TopNavBar } from '../_components/navBar/TopNavBar';
 import { SecondaryPagesGradient } from '../_components/backgroundGradient/SecondaryPagesGradient';
 
@@ -14,14 +15,16 @@ export default function MainLayout({
   const isDashboardPage = pathname === '/' || pathname === '/dashboard';
 
   return (
-    <SidebarProvider>
-      <div className="flex h-screen w-full flex-col bg-[#FCFAF7]">
-        <TopNavBar />
-        <main className="relative flex-1 overflow-y-auto p-8 text-gray-900">
-          {!isDashboardPage && <SecondaryPagesGradient />}
-          <div className="relative z-10">{children}</div>
-        </main>
-      </div>
-    </SidebarProvider>
+    <AdminGuard>
+      <SidebarProvider>
+        <div className="flex h-screen w-full flex-col bg-[#FCFAF7]">
+          <TopNavBar />
+          <main className="relative flex-1 overflow-y-auto p-8 text-gray-900">
+            {!isDashboardPage && <SecondaryPagesGradient />}
+            <div className="relative z-10">{children}</div>
+          </main>
+        </div>
+      </SidebarProvider>
+    </AdminGuard>
   );
 }
